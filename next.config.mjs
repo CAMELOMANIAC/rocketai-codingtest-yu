82783c401d18fede9ae390d718140361fc761b0a
yugyusang <yugyusang12@gmail.com>
2024-09-21 15:05:43 +0900

    환경변수를 vercel에서 등록하도록 재 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 import pkg from "./next-i18next.config.js";
-import { loadEnvConfig } from "@next/env";
 
 const { i18n } = pkg;
-const projectDir = process.cwd(); // 프로젝트의 루트 디렉토리
-const dev = process.env.NODE_ENV !== "production"; // 개발 모드인지 확인
-
-loadEnvConfig(projectDir, dev);
 
 const config = {
   i18n,
